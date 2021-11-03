e90e50ba7b5f70513a074fb35119c8c3b7ae8ce9
losuler <losuler@posteo.net>
2021-11-04 10:10:36 +1100

    Remove MicroG, hosts and AOD setting

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
 cd "${AOSP_BUILD_DIR}"
-
-echo "applying microg patches"
-cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
-patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
-cd "${AOSP_BUILD_DIR}/frameworks/base"
-patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
-
-# apply custom hosts file
-custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-echo "applying custom hosts file ${custom_hosts_file}"
-retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
